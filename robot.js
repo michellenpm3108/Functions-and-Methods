e00e8bf6b52c844d29ce0737ad9b9bd6ea452a40
Michelle <michelle.npm3108@gmail.com>
2020-11-24 07:21:18 +0800

    add js to move monster

--- robot.js
+++ robot.js
@@ -5,3 +5,10 @@ function moveRobotRed() {
     robotRed.style.left = number + 'px'
 }
 robotRed.addEventListener('click', moveRobotRed)
+
+const robotYellow = document.querySelector('.robot-yellow')
+function moveRobotYellow() {
+    number +=50
+    robotYellow.style.right = number + 'px'
+}
+robotYellow.addEventListener('click', moveRobotYellow)
